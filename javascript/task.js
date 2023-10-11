1aedc12b8249d3108b519b948e869d1b73aaa3a6
Alessandro Angheben <aleangheben@hotmail.com>
2023-10-11 15:46:15 +0200

    miss only css part

---
+++ javascript/task.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+  const main = document.getElementsByTagName("main")[0];
+  const section = document.getElementById("mySection");
+  const subBtn = document.getElementById("subButton");
+
+  const addProducts = function () {
+    const productInput = document.getElementById("product");
+
+    const myList = document.getElementById("myList");
+    const myLi = document.createElement("li");
+    myLi.innerText = productInput.value;
+    productInput.value = "";
+    myList.appendChild(myLi);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.onclick = function (e) {
+      const clickedDeleteBtn = e.target;
+      clickedDeleteBtn.parentNode.remove();
+    };
+    myLi.appendChild(deleteBtn);
+    myLi.onclick = () => {
+      console.log(myLi);
+      myLi.style.textDecoration = "line-through";
+    };
+  };
+  subBtn.addEventListener("click", addProducts);
+};
